14474abf1bc15dff329593627aa246c4245e6c94
sam <samuel13241@gmail.com>
2024-05-08 00:24:36 -0700

    no message

--- src/main/java/com/example/ItSolutionCore/common/config/CorsMvcConfig.java
+++ src/main/java/com/example/ItSolutionCore/common/config/CorsMvcConfig.java
@@ -1,20 +1,20 @@
-package com.example.ItSolutionCore.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-
-        corsRegistry.addMapping("/**")
-                .exposedHeaders("*")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowedOrigins("*");
-    }
-
-}
+//package com.example.ItSolutionCore.common.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsMvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry corsRegistry) {
+//
+//        corsRegistry.addMapping("/**")
+//                .exposedHeaders("*")
+//                .allowedHeaders("*")
+//                .allowedMethods("*")
+//                .allowedOrigins("*");
+//    }
+//
+//}
