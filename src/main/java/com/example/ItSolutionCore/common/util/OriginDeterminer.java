7f321bf5130ee8da11146db23455cb47ea73f000
sam <samuel13241@gmail.com>
2024-05-08 13:22:33 -0700

    no message

--- src/main/java/com/example/ItSolutionCore/common/util/OriginDeterminer.java
+++ src/main/java/com/example/ItSolutionCore/common/util/OriginDeterminer.java
@@ -2,11 +2,13 @@ package com.example.ItSolutionCore.common.util;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class OriginDeterminer {
     @Value("${redirection.url.itSolution}")
     private String itSolution_url;
@@ -20,11 +22,12 @@ public class OriginDeterminer {
         switch (origin){
 
             case "itSolution":
+                log.info(itSolution_url);
                 return itSolution_url;
 
             case "sunriseCC":
+                log.info(sunriseCC_url);
                 return sunriseCC_url;
-
             default: return null;
         }
     }
